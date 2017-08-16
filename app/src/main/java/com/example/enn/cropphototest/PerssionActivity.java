f99c8609891511d7befd0071ee79ce017e23e0ac
wxs <1084733625@qq.com>
2017-08-16 16:24:33 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/example/enn/cropphototest/PerssionActivity.java
@@ -0,0 +1,40 @@
+package com.example.enn.cropphototest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by wxs on 2017/8/16.
+ */
+
+public class PerssionActivity extends BaseActivity {
+
+
+    private void pickFromGallery() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE,
+                    "读入文件权限",
+                    REQUEST_STORAGE_READ_ACCESS_PERMISSION);
+        } else {
+            /*to do*/
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_STORAGE_READ_ACCESS_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickFromGallery();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
